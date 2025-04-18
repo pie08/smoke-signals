fa3efb6794e8645f89e38c03e60f421bbb9f47eb
Tyrus <tyrusbrianberggren@gmail.com>
2025-04-18 10:18:17 -0400

    use next/image to load hero image

--- app/(routes)/(home)/_components/Hero.tsx
+++ app/(routes)/(home)/_components/Hero.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import styles from "./Hero.module.scss";
 import Container from "@/app/_components/Container";
 import Section from "@/app/_components/Section";
+import Image from "next/image";
+import heroImage from "../../../../public/assets/images/shop/SMOKESIGNALS-48.jpg";
 
 type HeroProps = object;
 
@@ -12,6 +14,8 @@ const Hero: FC<HeroProps> = ({}) => {
         <h1>SMOKIN&apos; GOOD SINCE 2001</h1>
         <p>EVERYTHING YOU NEED FOR A HIGH QUALITY EXPERIENCE</p>
       </Container>
+
+      <Image src={heroImage} alt="Image of smoke signals shop" fill />
     </Section>
   );
 };
