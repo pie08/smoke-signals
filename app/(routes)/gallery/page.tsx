05ae11b5a670eba0337a1ae0eb564c5db42fc9f7
Tyrus Berggren <tyrusbrianberggren@gmail.com>
2025-04-18 18:56:53 -0400

    fix getAllImagePaths function

--- app/(routes)/gallery/page.tsx
+++ app/(routes)/gallery/page.tsx
@@ -3,14 +3,14 @@ import SubpageHeading from "@/app/_components/subpage/SubpageHeading";
 import SubpageSection from "@/app/_components/subpage/SubpageSection";
 import { FC } from "react";
 import Gallery from "./Gallery";
-import getAllFilePaths from "@/app/_lib/getAllFilePaths";
+import getAllImagePaths from "@/app/_lib/getAllImagePaths";
 
 type pageProps = object;
 
 // ! OPTIMIZE: ADD IMAGE LOADING ANIMATION
 const page: FC<pageProps> = async ({}) => {
   // get all images
-  const imagePaths = await getAllFilePaths("/public/assets/images");
+  const imagePaths = await getAllImagePaths("/public/assets/images");
 
   return (
     <SubpageSection fill>
---
+++ app/_lib/getAllImagePaths.ts
@@ -0,0 +1,63 @@
+import { readdir, stat } from "fs/promises";
+import { glob } from "glob";
+import { imageSizeFromFile } from "image-size/fromFile";
+
+async function addAllFilesRecursive(rootPath: string, pathArr: string[] = []) {
+  try {
+    // get all files
+    const files = await readdir(rootPath);
+
+    // add all non directories to arr
+    for (const path of files) {
+      const stats = await stat(rootPath + "/" + path);
+
+      if (stats.isFile()) {
+        pathArr.push(rootPath + "/" + path);
+      }
+    }
+
+    // recursively call this with cild directory
+    for (const path of files) {
+      const stats = await stat(rootPath + "/" + path);
+
+      if (stats.isDirectory()) {
+        await addAllFilesRecursive(rootPath + "/" + path, pathArr);
+      }
+    }
+
+    return pathArr;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function getAllImagePaths(rootDir: string) {
+  const parentPath = process.cwd() + rootDir;
+
+  const files = await addAllFilesRecursive(parentPath);
+
+  const filesWithData = files.map(async (path) => {
+    try {
+      if (path.split("/").at(-1)![0] === ".") return;
+
+      const { width, height } = await imageSizeFromFile(path);
+      return {
+        path: path.split("public")[1].replaceAll("\\", "/"),
+        width,
+        height,
+      };
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  });
+
+  // filter out undefined
+  const output = (await Promise.all(filesWithData)).filter(
+    (data) => data !== undefined
+  );
+  return output;
+}
+
+export default getAllImagePaths;
