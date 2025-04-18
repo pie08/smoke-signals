f110e95e4611516e87d78cc7ed443c7d4ab3960f
Tyrus <tyrusbrianberggren@gmail.com>
2025-04-18 09:41:46 -0400

    change getAllFilePaths to use own recursive function

--- app/_lib/getAllFilePaths.ts
+++ app/_lib/getAllFilePaths.ts
@@ -1,49 +1,45 @@
-import { statSync } from "fs";
 import { readdir, stat } from "fs/promises";
 import { glob } from "glob";
 import { imageSizeFromFile } from "image-size/fromFile";
 
-async function addAllFilesRecursive(rootPath: string, pathArr: string[]) {
+async function addAllFilesRecursive(rootPath: string, pathArr: string[] = []) {
   // get all files
   const files = await readdir(rootPath);
 
-  // get all directories
-  const directories = await glob(rootPath + "/**/*", {
-    nodir: true,
-  });
-  console.log(directories);
+  // add all non directories to arr
+  for (const path of files) {
+    const stats = await stat(rootPath + "/" + path);
 
-  // // add all non directories to arr
-  // files.forEach((path) => {
-  //   const stats = statSync(rootPath + "/" + path);
+    if (stats.isFile()) {
+      pathArr.push(rootPath + "/" + path);
+    }
+  }
 
-  //   if (stats.isFile()) {
-  //     pathArr.push(path);
-  //   }
-  // });
+  // recursively call this with cild directory
+  for (const path of files) {
+    const stats = await stat(rootPath + "/" + path);
 
-  // // recursively call this with cild directory
-  // files.forEach((path) => {
-  //   const stats = statSync(rootPath + "/" + path);
+    if (stats.isDirectory()) {
+      await addAllFilesRecursive(rootPath + "/" + path, pathArr);
+    }
+  }
 
-  //   if (stats.isDirectory()) {
-  //     addAllFilesRecursive(rootPath + "/" + path, pathArr);
-  //   }
-  // });
+  return pathArr;
 }
 
 async function getAllFilePaths(rootDir: string) {
   const parentPath = process.cwd() + rootDir;
 
-  const files = await glob(parentPath + "/**/*", {
-    nodir: true,
-  });
+  const files = await addAllFilesRecursive(parentPath);
+  console.log(files);
 
   const filesWithData = files.map(async (path) => {
-    const { width, height } = await imageSizeFromFile(
-      process.cwd() + "/" + path
-    );
-    return { path: "/" + path.slice(7).replaceAll("\\", "/"), width, height };
+    const { width, height } = await imageSizeFromFile(path);
+    return {
+      path: path.split("public")[1].replaceAll("\\", "/"),
+      width,
+      height,
+    };
   });
 
   return Promise.all(filesWithData);
